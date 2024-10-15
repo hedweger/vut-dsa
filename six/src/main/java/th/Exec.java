66c642b201e08aea0b0eda3473d2874918bb24e6
th <73221181+hedweger@users.noreply.github.com>
2024-10-15 18:41:12 +0200

    Update Exec.java

--- six/src/main/java/th/Exec.java
+++ six/src/main/java/th/Exec.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package th;
+package cviok;
 
 /**
  *
@@ -13,15 +13,16 @@ public class Exec {
     public static void main(String[] args) {
         BinTree bt = new BinTree();
         bt.add(7);
-        bt.add(1);
         bt.add(5);
-        bt.add(6);
         bt.add(14);
-        bt.add(10);
+        bt.add(1);
+        bt.add(6);
         bt.add(16);
+        bt.add(10);
         bt.print_inorder();
         bt.print_preorder();
         bt.print_postorder();
+        bt.print_leaves();
         System.out.println(bt.contains(1));
         System.out.println(bt.contains(12));
         System.out.println(bt.contains(9));
